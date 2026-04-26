7659c45ce21003caf21427fb9638e4b0394d6ef7
rasal8 <mobileaccessories122@gmail.com>
2026-04-26 19:03:46 +0530

    Add /generate route to serve drama HTML
    Added a new route to generate HTML for a drama page.

--- server.js
+++ server.js
@@ -12,7 +12,24 @@ app.get("/", (req, res) => {
 app.get("/test", (req, res) => {
   res.json({ status: "working" });
 });
+app.get("/generate", (req, res) => {
+  const drama = req.query.name || "Unknown Drama";
 
+  const html = `
+    <h1>🎬 ${drama}</h1>
+    <p>🔥 Watch ${drama} Online</p>
+
+    <h2>🛍 Shop Inspired Products</h2>
+    <ul>
+      <li><a href="#">Korean Outfit</a></li>
+      <li><a href="#">Accessories</a></li>
+    </ul>
+
+    <p>👉 Powered by KDrama Affiliate System</p>
+  `;
+
+  res.send(html);
+});
 app.listen(PORT, () => {
   console.log("Server running on " + PORT);
 });
