1bed6f2508c0539f5ea6080e2dd5cc6911cc8ca9
rasal8 <mobileaccessories122@gmail.com>
2026-04-27 14:56:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.get("/outfit", (req, res) => {
 app.get("/accessories", (req, res) => {
   const drama = req.query.name;
 
-  const keyword = "korean fashion accessories women";
+  const keyword = getAccessoryKeyword(drama); ✅
 
   const link = `https://www.amazon.in/s?k=${encodeURIComponent(keyword)}&tag=rrasal-21&linkCode=ll1`;
 
