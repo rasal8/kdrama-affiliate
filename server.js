b76b77d84dc72336c82a2dff2dd8d65cc756140b
rasal8 <mobileaccessories122@gmail.com>
2026-04-27 14:10:11 +0530

    Update keyword generation for accessories route

--- server.js
+++ server.js
@@ -179,7 +179,7 @@ app.get("/outfit", (req, res) => {
 app.get("/accessories", (req, res) => {
   const drama = req.query.name;
 
-  const keyword = "korean accessories fashion";
+  const keyword = `${drama} korean accessories`;
 
   const link = `https://www.amazon.in/s?k=${encodeURIComponent(keyword)}&tag=rrasal-21&linkCode=ll1`;
 
