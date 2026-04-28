e4306bb58a5a59cc2c39c1e376b48bfb898895e6
rasal8 <mobileaccessories122@gmail.com>
2026-04-28 10:06:50 +0530

    Add getTopProducts function for product suggestions

--- server.js
+++ server.js
@@ -341,6 +341,22 @@ const accessoryKeyword = getAccessoryKeyword(drama);
 
 const outfitLink = getAmazonLink(outfitKeyword);
 const accessoryLink = getAmazonLink(accessoryKeyword);
+    function getTopProducts(keyword) {
+  return [
+    {
+      title: "Korean Style Oversized Hoodie",
+      link: getAmazonLink(keyword + " hoodie"),
+    },
+    {
+      title: "Korean Aesthetic Sweater",
+      link: getAmazonLink(keyword + " sweater"),
+    },
+    {
+      title: "Korean Fashion Jacket",
+      link: getAmazonLink(keyword + " jacket"),
+    }
+  ];
+    }
 
     const caption = `
 🎬 ${d.Title || drama}
