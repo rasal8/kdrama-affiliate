555cf4157465612c437c67623950393fcb8bd135
rasal8 <mobileaccessories122@gmail.com>
2026-04-28 10:29:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -341,7 +341,7 @@ const accessoryKeyword = getAccessoryKeyword(drama);
 
 const outfitLink = getAmazonLink(outfitKeyword);
 const accessoryLink = getAmazonLink(accessoryKeyword);
-    const products = getTopProducts(outfitKeyword);
+    const products = getTopProducts(Keyword);
 
     const caption = `
 🎬 ${d.Title || drama}
